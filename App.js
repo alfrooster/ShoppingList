ea39dde5bd335136d16feb4ceadd3fc2e9f13985
Alfred <alfred.ahonen@gmail.com>
2023-03-14 17:03:01 +0200

    firebase

--- App.js
+++ App.js
@@ -1,41 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
-import * as SQLite from 'expo-sqlite';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
 
-const db = SQLite.openDatabase('ostosdb');
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: firebase_apiKey,
+  authDomain: firebase_authDomain,
+  databaseURL: firebase_databaseURL,
+  projectId: firebase_projectId,
+  storageBucket: firebase_storageBucket,
+  messagingSenderId: firebase_messagingSenderId,
+  appId: firebase_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 export default function App() {
   const [amount, setAmount] = useState('');
   const [name, setName] = useState('');
   const [products, setProducts] = useState([]);
+  const [keys, setKeys] = useState([]);
 
+  const itemsRef = ref(database, 'items/');
+
+  //jos poistaa kaikki listasta, tulee erroria, en saanu korjattua
   useEffect(() => {
-    db.transaction(tx => {
-    tx.executeSql('create table if not exists product (id integer primary key not null, amount text, name text);');
-    }, null, updateList);
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      setProducts(Object.values(data));
+      setKeys(Object.keys(data));
+    })
   }, []);
 
   const saveItem = () => {
-    db.transaction(tx => {
-      tx.executeSql('insert into product (amount, name) values (?, ?);',
-      [amount, name]);
-    }, null, updateList);
+    push(
+      itemsRef, {'name': name, 'amount': amount}
+    )
     setAmount('');
     setName('');
   }
 
-  const updateList = () => {
-    db.transaction(tx => {
-      tx.executeSql('select * from product;', [], (_, { rows }) =>
-        setProducts(rows._array)
-      );
-    }, null, null);
-  }
-
   const deleteItem = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('delete from product where id = ?;', [id]);
-    }, null, updateList)
+    remove(ref(database, 'items/' + keys[id]));
   }
     
   return (
@@ -53,11 +62,10 @@ export default function App() {
       <Button onPress={saveItem} title="Save" />
       <Text style={{marginTop: 15, fontSize: 22}}>Shopping list</Text>
       <FlatList
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) =>
+        renderItem={({item, index}) =>
           <View style={styles.listcontainer}>
             <Text style={{fontSize: 20}}>{item.name}, {item.amount} </Text>
-            <Text style={{color: '#0000ff', fontSize: 20}} onPress={() => deleteItem(item.id)}> bought</Text>
+            <Text style={{color: '#0000ff', fontSize: 20}} onPress={() => deleteItem(index)}> bought</Text>
           </View>}
         data={products}
       />
